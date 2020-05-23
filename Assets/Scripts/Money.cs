6a410fd231d8e193bbe9c8385f41699135ed6bff
Thomas Zhang <thomaszhang@berkeley.edu>
2020-05-22 23:39:48 -0700

    Added very simple playerprefs abstraction class to handle all the saves and added upgrades screen with money

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Money : MonoBehaviour
+{
+    private TextMeshProUGUI moneyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyText = GetComponent<TextMeshProUGUI>();
+        UpdateMoneyDisplay();
+    }
+
+    private void UpdateMoneyDisplay()
+    {
+        moneyText.text = $"${PlayerPrefsManager.Money}";
+    }
+
+    public void SubtractMoney(int amount)
+    {
+        if (amount <= PlayerPrefsManager.Money)
+        {
+            PlayerPrefsManager.Money -= amount;
+        }
+        UpdateMoneyDisplay();
+    }
+
+    public void AddMoney(int amount)
+    {
+        PlayerPrefsManager.Money += amount;
+        UpdateMoneyDisplay();
+    }
+}
---
+++ Assets/Scripts/SaveSystem/PlayerPrefsManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PlayerPrefsManager : MonoBehaviour
+{
+    public static int Money
+    {
+        get { return PlayerPrefs.GetInt(nameof(Money), 0); }
+        set { PlayerPrefs.SetInt(nameof(Money), value); }
+    }
+}
