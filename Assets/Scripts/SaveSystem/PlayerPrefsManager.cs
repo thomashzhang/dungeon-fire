ec86af0e06241bb0e574690a38406d5969f6eeb7
Thomas Zhang <thomaszhang@berkeley.edu>
2020-05-24 20:40:06 -0700

    Reverted starting rubies count to 0

--- Assets/Scripts/SaveSystem/PlayerPrefsManager.cs
+++ Assets/Scripts/SaveSystem/PlayerPrefsManager.cs
@@ -6,7 +6,7 @@ public class PlayerPrefsManager : MonoBehaviour
     // WARNING, DO NOT RENAME THESE VARIABLES AS THERE ARE STRING REFERENCES
     public static int Rubies
     {
-        get { return PlayerPrefs.GetInt(nameof(Rubies), 1000); }
+        get { return PlayerPrefs.GetInt(nameof(Rubies), 0); }
         set { PlayerPrefs.SetInt(nameof(Rubies), value); }
     }
 
@@ -18,7 +18,7 @@ public class PlayerPrefsManager : MonoBehaviour
 
     public static int LivesUpgradeLevel
     {
-        get { return PlayerPrefs.GetInt(nameof(LivesUpgradeLevel), 15); }
+        get { return PlayerPrefs.GetInt(nameof(LivesUpgradeLevel), 19); }
         set { PlayerPrefs.SetInt(nameof(LivesUpgradeLevel), value); }
     }
 }
