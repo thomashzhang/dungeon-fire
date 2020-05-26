14da8772ce0611d222d84d3bfb645a435096a8c6
Thomas Zhang <thomaszhang@berkeley.edu>
2020-05-25 22:42:30 -0700

    Added tutorial to level 1

---
+++ Assets/Scripts/Level1Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void DoneTutorial()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
